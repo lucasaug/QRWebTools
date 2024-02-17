d616052427f7a871bbf30599111073b7f6d22110
Lucas Augusto <luc.aug.freire@gmail.com>
2024-02-16 21:53:07 -0300

    Include some usability improvements

--- src/main.py
+++ src/main.py
@@ -17,6 +17,9 @@ def index():
 @app.route("/create_code", methods=["POST"])
 def create_code():
     text = request.form["text"]
+    if not text:
+        return "<div></div>"
+
     pil_img = qrcode.make(text)
 
     img_io = BytesIO()
@@ -24,4 +27,9 @@ def create_code():
     img_io.seek(0)
 
     base64_img = base64.b64encode(img_io.getvalue()).decode("utf-8")
-    return f"<img src='data:image/png;base64, {base64_img}'/>"
+    return f"""
+        <img src='data:image/png;base64, {base64_img}'/>
+        <button onclick=
+            "window.location.assign('data:application/octet-stream;base64, {base64_img}');"
+        >Download image</button>
+    """
